36cffd945d3f12ab91f8924c193fed1c66a09def
Pterpsycore <pterrpsycore@gmail.com>
2019-02-01 16:57:04 +0300

    Initial commit

---
+++ main.lua
@@ -0,0 +1,33 @@
+os.setlocale('Russian_Russia.1251')
+
+local path = 'input.txt'
+local input = io.open(path, "r")
+-- Очистка текста от цифр и знаков препинания (кроме дефисов являющихся частью слова)
+local text = input:read("*a"):gsub('[,.?!)(:"%d]', ' '):gsub("%s+-", " "):gsub("-%s+", " ")
+local frequency = {}
+local words = {}
+
+input:close()
+
+for word in text:gmatch("%S+") do
+    word = word:lower()
+    if frequency[word] then
+        frequency[word] = frequency[word] + 1
+    else
+        frequency[word] = 1
+        words[#words+1] = word
+    end
+end
+
+table.sort(words, function(a, b) return frequency[a] > frequency[b] end)
+
+local output = io.open('output.txt', 'w')
+
+for i, word in pairs(words) do
+    output:write(string.format("%u. %s (%u)\n", i, word, frequency[word]))
+    if i == 100 then
+        break
+    end
+end
+
+output:close()
