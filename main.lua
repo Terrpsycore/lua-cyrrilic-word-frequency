c74bd5e15d4fae0cbfcb7d4cef92b6537a688717
Terrpsycore <47067496+Terrpsycore@users.noreply.github.com>
2019-02-09 17:27:13 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,15 +1,15 @@
 os.setlocale('Russian_Russia.1251')
 
 local path = 'input.txt'
-local input = io.open(path, "r")
+local input = io.open(path, 'r')
 -- Deleting all numbers and punctuation marks except word-part hyphens
-local text = input:read("*a"):gsub('[,.?!)(:"%d]', ' '):gsub("%s+-", " "):gsub("-%s+", " ")
+local text = input:read('*a'):gsub('[,.?!)(:"%d]', ' '):gsub('%s+-', " "):gsub('-%s+', ' ')
 local frequency = {}
 local words = {}
 
 input:close()
 
-for word in text:gmatch("%S+") do
+for word in text:gmatch('%S+') do
     word = word:lower()
     if frequency[word] then
         frequency[word] = frequency[word] + 1
@@ -24,7 +24,7 @@ table.sort(words, function(a, b) return frequency[a] > frequency[b] end)
 local output = io.open('output.txt', 'w')
 
 for i, word in pairs(words) do
-    output:write(string.format("%u. %s (%u)\n", i, word, frequency[word]))
+    output:write(string.format('%u. %s (%u)\n', i, word, frequency[word]))
     if i == 100 then
         break
     end
